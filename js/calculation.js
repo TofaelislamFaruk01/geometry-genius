df294b97404f40d3446eee49a4684618a550bd4f
TofaelislamFaruk01 <tofaelfaruk@gmail.com>
2023-10-04 03:17:16 +0600

    show values in the box .. little added not finished

--- js/calculation.js
+++ js/calculation.js
@@ -1,1 +1,116 @@
 console.log("calc");
+let cnt = 0;
+
+// triangle button
+
+document.getElementById('calcT').addEventListener('click', function () {
+    
+    const baseT = getValueById('baseT');
+    console.log(baseT);
+    const heightT = getValueById('heightT');
+    console.log(heightT);
+    const flag= check(baseT, heightT);
+    if (flag ===1) {
+        let area = 0.5 * baseT * heightT;
+        area = area.toFixed(2);
+        cnt++;
+        console.log(area, cnt);
+        setValue(area, cnt, 'Triangle');
+        
+    }
+    empty('baseT','heightT');
+})
+
+// rectangle button
+
+document.getElementById('rectangle-btn').addEventListener('click', function () {
+    
+    const num1 = getValueById('num1');
+    console.log(num1);
+    const num2 = getValueById('num2');
+    console.log(num2);
+    const flag= check(num1, num2);
+    if (flag === 1) {
+        let area = num1 * num2;
+        area = area.toFixed(2);
+        cnt++;
+        console.log(area, cnt);
+        setValue(area, cnt, 'Rectangle');
+       
+    }
+     empty('num1','num2');
+})
+
+// parallelogram button
+document.getElementById('para-btn').addEventListener('click', function () {
+    
+    const num1 = getValueById('baseP');
+    console.log(num1);
+    const num2 = getValueById('heightP');
+    console.log(num2);
+    const flag= check(num1, num2);
+    if (flag === 1) {
+        let area = num1 * num2;
+        area = area.toFixed(2);
+        cnt++;
+        console.log(area, cnt);
+        setValue(area, cnt, 'Parallelogram');
+        
+    }
+    empty('baseP','heightP');
+})
+// rhombos button
+document.getElementById('rhombus-btn').addEventListener('click', function () {
+    
+    const num1 = getValueById('d1R');
+    console.log(num1);
+    const num2 = getValueById('d2R');
+    console.log(num2);
+    const flag= check(num1, num2);
+    if (flag === 1) {
+        let area = 0.5 * num1 * num2;
+        area = area.toFixed(2);
+       cnt++;
+        console.log(area, cnt);
+        setValue(area, cnt, 'Rhombus');
+       
+    }
+     empty('d1R','d2R');
+})
+// pentagon button
+document.getElementById('pentagon-btn').addEventListener('click', function () {
+    
+     const num1 = getValueById('pen1');
+    console.log(num1);
+     const num2 = getValueById('pen2');
+     console.log(num2);
+   
+    const flag= check(num1, num2);
+    if (flag === 1) {
+        let area = 0.5 * num1 * num2;
+        area = area.toFixed(2);
+         cnt++;
+        console.log(area, cnt);
+        setValue(area, cnt, 'Pentagon');
+        
+    }
+    empty('pen1','pen2');
+})
+// ellipse button
+document.getElementById('ellipse-btn').addEventListener('click', function () {
+    
+    const num1 = getValueById('ell1');
+    console.log(num1);
+    const num2 = getValueById('ell2');
+    console.log(num2);
+    const flag= check(num1, num2);
+    if (flag === 1) {
+        let area = 3.1416 * num1 * num2;
+        area = area.toFixed(2);
+         cnt++;
+        console.log(area, cnt);
+        setValue(area, cnt, 'Ellipse');
+        
+    }
+    empty('ell1','ell2');
+})
--- utilities/utilities.js
+++ utilities/utilities.js
@@ -1,1 +1,66 @@
 console.log(" utilities");
+
+
+// get values from text input
+
+function getValueById(boxId)
+{
+    const boxValue = document.getElementById(boxId);
+  //  console.log(boxValue);
+    const boxValueString = boxValue.value;
+    const value = parseFloat(boxValueString);
+    return value;
+}
+ // check it is a positive number or not
+function check(num1, num2)
+{
+     if (num1 >= 0 && num2 >= 0) {
+         return 1;
+    }
+    else {
+        alert('please enter a positive number');
+    }
+
+}
+// set value by id
+
+function setValueById(boxid , value)
+{
+    const areaBoxId = document.getElementById(boxid);
+    areaBoxId.innerText = value;
+}
+
+// set the value inside the area
+
+function setValue(area, cnt, shape)
+{
+    const areaBox = document.getElementById('box-area');
+    console.log(areaBox);
+    const section = document.createElement('div');
+    section.innerHTML = `
+    
+     <p> <span id="cnt">1</span>.<span id="shape">shape</span> </p>
+     <p><span id="area">1200.98</span>cm<sup>2</sup> </p>
+    <button class="btn btn-primary area-btn" type="submit">convert to m<sup>2</sup> </button>
+    
+    `
+    areaBox.appendChild(section);
+   
+    section.classList.add("d-flex", "justify-content-between", "align-items-center");
+
+    setValueById('cnt', cnt);
+    setValueById('shape', shape);
+    setValueById('area', area);
+    return 0;
+}
+
+// empty to all
+function empty(box1,box2)
+{
+    const inputField1 = document.getElementById(box1);
+    inputField1.value = '';
+    const inputField2 = document.getElementById(box2);
+    inputField2.value = '';
+}
+
+
