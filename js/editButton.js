0434d63e23dbc20992833e1ec1a21dd6c2741193
TofaelislamFaruk01 <tofaelfaruk@gmail.com>
2023-10-08 01:47:43 +0600

    for edit the value editButton js file added

---
+++ js/editButton.js
@@ -0,0 +1,17 @@
+
+//parallelogram editor
+document.getElementById('para-edit').addEventListener('click',function(){
+    editBox('para-input');
+})
+// rhombus editor
+document.getElementById('rhombus-edit').addEventListener('click',function(){
+    editBox('rhombus-input');
+})
+// pentagon editor
+document.getElementById('pen-edit').addEventListener('click',function(){
+    editBox('pen-input');
+})
+// ellipse editor
+document.getElementById('ell-edit').addEventListener('click',function(){
+    editBox('ell-input');
+})
